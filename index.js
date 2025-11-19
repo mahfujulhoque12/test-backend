3f5147e0e9ccd2b22a6abf8ca4417bbb05987bd7
mahfujulhoque12 <mdmahfujulhoque123@gmail.com>
2025-11-19 12:22:36 +0600

    1stf

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 // import userRouter from "./routes/user.route.js";
-// import authRouter from "./routes/auth.route.js";
+import authRouter from "./routes/auth.route.js";
 // import listingRouter from "./routes/listing.route.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -57,8 +57,8 @@ try {
   console.log("MongoDB connected");
 
   // Add routes after DB connection
-  app.use("/api/user", userRouter);
-  // app.use("/api", authRouter);
+  // app.use("/api/user", userRouter);
+  app.use("/api", authRouter);
   // app.use("/api/listing", listingRouter);
 } catch (error) {
   console.error("Failed to connect to MongoDB:", error);
